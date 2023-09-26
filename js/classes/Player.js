bb7c4f2c43c11b51ae216497d4a27ee0568fb7d7
Priyanshu Bhandari <technopriyu@gmail.com>
2023-09-26 17:31:44 +0530

    added gravity

--- js/classes/Player.js
+++ js/classes/Player.js
@@ -4,20 +4,30 @@ class Player {
       x: 100,
       y: 100,
     };
+    this.velocity = {
+      x: 0,
+      y: 0,
+    }; // pixel / frame
     this.width = 100;
     this.height = 100;
     this.sides = {
       bottom: this.position.y + this.height,
     };
+    this.gravity = 1;
   }
+
   draw() {
     c.fillStyle = "red";
     c.fillRect(this.position.x, this.position.y, this.width, this.height);
   }
   update() {
-    if (this.sides.bottom < canvas.height) {
-      this.position.y++;
+    this.position.y += this.velocity.y;
+
+    // Above bottom of canvas
+    if (this.sides.bottom + this.velocity.y < canvas.height) {
+      // this.position.y++;
+      this.velocity.y += this.gravity; // pixel per frame
       this.sides.bottom = this.position.y + this.height;
-    }
+    } else this.velocity.y = 0;
   }
 }
