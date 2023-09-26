ce5ab71447bd184a9cbd904c3c6cdabc948442bc
Priyanshu Bhandari <technopriyu@gmail.com>
2023-09-26 17:24:52 +0530

    added a player

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
+// js game engiine like phasor etc.
+// gsap or framermotion or anime for animation
 const canvas = document.querySelector("canvas");
+// canvas.style.
 const c = canvas.getContext("2d");
 
 // 16:9 ratio
@@ -6,5 +9,17 @@ canvas.width = 64 * 16; //1024;
 canvas.height = 64 * 9; //576;
 // our game will use 64 pixe x 64 pixels tiles
 
-c.fillStyle = "white";
-c.fillRect(0, 0, canvas.width, canvas.height);
+const player = new Player();
+function animate() {
+  // https://stackoverflow.com/questions/19764018/controlling-fps-with-requestanimationframe
+  window.requestAnimationFrame(animate);
+  // clear canvas for every frame of animation
+  // c.clearRect(0, 0, canvas.width, canvas.height); it gets clear of everything like bg color
+  c.fillStyle = "white";
+  c.fillRect(0, 0, canvas.width, canvas.height);
+  player.draw();
+  player.update();
+}
+// https://stackoverflow.com/questions/21064101/understanding-offsetwidth-clientwidth-scrollwidth-and-height-respectively
+
+animate();
